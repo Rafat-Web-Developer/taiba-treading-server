899508f73b731726b37aa38e323b1c26049511d3
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-07 06:17:30 +0600

    Create a get api for collect single item's data from database and it in clint site

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
       const cursor = itemsCollection.find(query);
       const items = await cursor.toArray();
       res.send(items);
+    });
+
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemsCollection.findOne(query);
+      res.send(item);
     });
 
     // POST API
